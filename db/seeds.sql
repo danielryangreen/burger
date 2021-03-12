eee8198c127ad9b5efe1eb673ef09fbc3bea81b2
Dan Green <dan.ryan.green@gmail.com>
2021-03-11 19:36:09 -0700

    DB setup

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Dairy Queen", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Steak 'n Shake", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Five Guys", false);
